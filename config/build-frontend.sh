72a439de8d9dc90e6761cbbacf4c809145e11674
yxzlwz <danny070601@aliyun.com>
2023-08-16 00:13:28 +0800

    [update] remove config file from frontend

--- config/build-frontend.sh
+++ config/build-frontend.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 cd ./frontend
-cat >config.json<<EOF
-{
-  "name": "$SITE_NAME",
-  "title": "$SITE_TITLE",
-  "useFooter": $USE_FOOTER,
-  "footer": {
-    "icp": "$FOOTER_ICP"
-  },
-  "allowRegister": $ALLOW_REGISTER,
-  "sentenceApi": "$SENTENCE_PROVIDER"
-}
-EOF
 yarn build
 rm -rf /root/frontend-dist/*
 cp -r ./dist/* /root/frontend-dist/
