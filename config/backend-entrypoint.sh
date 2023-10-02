4921af871d4b2c0a14e6097dae75f19c795c0958
yxzlwz <danny070601@aliyun.com>
2023-10-02 19:31:08 +0800

    [update] add .env file & use mirror option

--- config/backend-entrypoint.sh
+++ config/backend-entrypoint.sh
@@ -3,23 +3,22 @@
 cd /srv/server
 
 if [ ! -s /srv/server/secret.key ]; then
-
-        echo $(python3 -c "from django.core.management import utils;print(utils.get_random_secret_key())") > secret.key
-        export DJANGO_SUPERUSER_USERNAME=root
-        export DJANGO_SUPERUSER_PASSWORD=123456
-        export DJANGO_SUPERUSER_EMAIL=root@genuine.oj
-        python3 manage.py createsuperuser --no-input
+    echo $(python3 -c "from django.core.management import utils;print(utils.get_random_secret_key())") >secret.key
+    export DJANGO_SUPERUSER_USERNAME=$SUPERUSER_USERNAME
+    export DJANGO_SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD
+    export DJANGO_SUPERUSER_EMAIL=$SUPERUSER_EMAIL
+    python3 manage.py createsuperuser --no-input
 fi
 
 n=0
 while [ $n -lt 5 ]; do
     python3 manage.py makemigrations oj_user
     python3 manage.py migrate
-    python3 manage.py makemigrations oj_problem oj_submission oj_contest
+    python3 manage.py makemigrations oj_problem oj_submission oj_contest oj_discussion
     python3 manage.py migrate
     python3 manage.py collectstatic --noinput
     break
-    n=$(($n+1))
+    n=$(($n + 1))
     echo "Failed to migrate, going to retry..."
     sleep 8s
 done
