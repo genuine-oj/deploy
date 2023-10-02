ff35a1c86ac5382a5bd17b98c722a2a12e1ef11d
yxzlwz <danny070601@aliyun.com>
2023-10-02 12:52:59 +0800

    [update]

--- config/backend-entrypoint.sh
+++ config/backend-entrypoint.sh
@@ -3,23 +3,25 @@
 cd /srv/server
 
 if [ ! -s /srv/server/secret.key ]; then
-    n=0
-    while [ $n -lt 5 ]; do
+
         echo $(python3 -c "from django.core.management import utils;print(utils.get_random_secret_key())") > secret.key
-        python3 manage.py makemigrations oj_user
-        python3 manage.py migrate
-        python3 manage.py makemigrations oj_problem oj_submission oj_contest
-        python3 manage.py migrate
-        python3 manage.py collectstatic --noinput
         export DJANGO_SUPERUSER_USERNAME=root
         export DJANGO_SUPERUSER_PASSWORD=123456
         export DJANGO_SUPERUSER_EMAIL=root@genuine.oj
         python3 manage.py createsuperuser --no-input
-        break
-        n=$(($n+1))
-        echo "Failed to migrate, going to retry..."
-        sleep 8s
-    done
 fi
+
+n=0
+while [ $n -lt 5 ]; do
+    python3 manage.py makemigrations oj_user
+    python3 manage.py migrate
+    python3 manage.py makemigrations oj_problem oj_submission oj_contest
+    python3 manage.py migrate
+    python3 manage.py collectstatic --noinput
+    break
+    n=$(($n+1))
+    echo "Failed to migrate, going to retry..."
+    sleep 8s
+done
 
 exec supervisord -c /srv/supervisord.conf
