62a094c5b327b25717bb966bc966673132c5ba69
yxzlwz <danny070601@aliyun.com>
2023-10-02 12:05:44 +0800

    [update]

--- config/judger-config.py
+++ config/judger-config.py
@@ -7,6 +7,7 @@ PARALLEL_TESTS = int(os.getenv('PARALLEL_TESTS', 2))
 PARALLEL_USERS = int(os.getenv('PARALLEL_USERS', 1))
 
 BASE_DIR = Path('/judger').resolve()
+DEBUG = False
 
 RUN_USER_UID = pwd.getpwnam('code').pw_uid
 RUN_GROUP_GID = grp.getgrnam('code').gr_gid
@@ -18,3 +19,4 @@ SPJ_USER_UID = pwd.getpwnam('spj').pw_uid
 SPJ_GROUP_GID = grp.getgrnam('spj').gr_gid
 
 TEST_CASE_DIR = Path(os.getenv('TEST_CASE_DIR', '/srv/test_data')).resolve()
+SPJ_DIR = Path(os.getenv('SPJ_DIR', '/srv/spj')).resolve()
